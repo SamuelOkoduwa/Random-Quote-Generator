4a25658698d39583897a2b869a52125ddb36e938
Suboms <40143366+Suboms@users.noreply.github.com>
2024-02-16 10:37:17 +0100

    minor change

--- assets/index.js
+++ assets/index.js
@@ -1,72 +1,54 @@
-let btn = document.querySelector("button");
 let hero = document.querySelector(".hero");
+hero.style.display = "none";
 let loader = document.querySelector(".loader");
 
-hero.style.display = "none"
-
 async function quote() {
-  let url = `https://api.seriesquotes.10cyrilc.me/quote/`;
-
+  let url = `https://api.api-ninjas.com/v1/quotes`;
   const response = await fetch(url, {
     method: "GET",
+    headers: {
+      "Content-Type": "application/json",
+      "X-Api-Key": "VT6Z7rKdInt3F8kOrNmuKA==dhTFCZac9OjS5d8Z",
+    },
   });
-
   const data = await response.json();
-
   return data;
 }
 
+let btn = document.querySelector("button");
 
 async function changeBg() {
   let countdown = 3;
-
   const quoteData = await quote();
+  const quoter = document.getElementById("quoter");
+  const quoteText = document.getElementById("quote");
 
   if (quoteData[0] != undefined) {
-    let words = quoteData[0].quote.split(" ");
+    setInterval(function () {
+      countdown -= 1;
+      if (countdown <= 0) {
+        clearInterval();
 
-    console.log(words.length);
-
-    if (words.length > 65) {
-      changeBg();
-    }
-    else {
-      setInterval(function () {
-        countdown -= 1;
-
-        if (countdown <= 0) {
-          clearInterval();
-
-          loader.style.display = "none";
-          hero.style.display = "flex";
-        }
-      }, 1000);
-
-      document.getElementById("quote").textContent = quoteData[0].quote;
-      document.getElementById("quoter").textContent = `- ${quoteData[0].author}`;
-    }
+        loader.style.display = "none";
+        hero.style.display = "flex";
+      }
+    }, 1000);
   }
+
+  setInterval(async function () {
+    const quoteData = await quote();
+    let words = quoteData[0].quote.split(" ");
+    // console.log(words);
+    // console.log(words.length);
+    quoteText.textContent = quoteData[0].quote;
+    quoter.textContent = `- ${quoteData[0].author}`;
+    if (words.length <= 15) {
+      quoteText.style.fontSize = "1.5rem";
+    }else if(words.length > 15 && words.length <= 70){
+      quoteText.style.fontSize = "1rem";}
+
+  }, 10000);
 }
 
-let imgBtn = document.getElementById("reload");
-
 btn.addEventListener("click", changeBg);
-
-btn.addEventListener("click", () => {
-  let anime = 2;
-
-  imgBtn.classList.add("reload");
-
-  document.getElementsByClassName("reload")[0].style.animation = "rotate 1s ease-in-out 1";
-
-  setInterval(function () {
-    anime -= 1;
-
-    if (anime == 0) {
-      document.getElementsByClassName("reload")[0].style.animation = "";
-      clearInterval();
-    }
-  }, 1000);
-});
-
 window.onload = changeBg();
