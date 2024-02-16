e575c98b9609c6952c0b09319ea91af74f696f3c
David Adeshina <49479389+DarkShadow77@users.noreply.github.com>
2024-02-16 01:26:49 -0800

    Add files via upload

--- assets/index.js
+++ assets/index.js
@@ -1,4 +1,4 @@
-
+let btn = document.querySelector("button");
 let hero = document.querySelector(".hero");
 let loader = document.querySelector(".loader");
 
@@ -6,57 +6,67 @@ hero.style.display = "none"
 
 async function quote() {
   let url = `https://api.seriesquotes.10cyrilc.me/quote/`;
+
   const response = await fetch(url, {
     method: "GET",
-    headers: {
-      "Content-Type": "application/json",
-    },
-  })
+  });
+
   const data = await response.json();
-  console.log(data);
+
   return data;
 }
 
-let btn = document.querySelector("button");
 
 async function changeBg() {
-    let countdown = 3;
-    const quoteData = await quote();
+  let countdown = 3;
 
-    if (quoteData[0] != undefined) {
-      setInterval(function () {
+  const quoteData = await quote();
 
-        countdown -= 1
-        if (countdown <= 0) {
-          clearInterval()
+  if (quoteData[0] != undefined) {
+    let words = quoteData[0].quote.split(" ");
 
-          loader.style.display = "none"
-          hero.style.display = "flex"
-        }
-      }, 1000)
+    console.log(words.length);
+
+    if (words.length > 65) {
+      changeBg();
     }
+    else {
+      setInterval(function () {
+        countdown -= 1;
 
-    document.getElementById("quote").textContent = quoteData[0].quote;
-    document.getElementById("quoter").textContent = `- ${quoteData[0].author}`;
+        if (countdown <= 0) {
+          clearInterval();
+
+          loader.style.display = "none";
+          hero.style.display = "flex";
+        }
+      }, 1000);
+
+      document.getElementById("quote").textContent = quoteData[0].quote;
+      document.getElementById("quoter").textContent = `- ${quoteData[0].author}`;
+    }
+  }
 }
-let imgBtn =  document.getElementById("reload")
+
+let imgBtn = document.getElementById("reload");
+
+btn.addEventListener("click", changeBg);
+
 btn.addEventListener("click", () => {
   let anime = 2;
 
   imgBtn.classList.add("reload");
-  document.getElementsByClassName("reload")[0].style.animation = "";
+
   document.getElementsByClassName("reload")[0].style.animation = "rotate 1s ease-in-out 1";
-  // setInterval(function () {
 
-  //   anime -= 1
-  //   if (anime <= 0) {
-  //     document.getElementsByClassName("reload")[0].style.animation = ""
-  //     imgBtn.classList.remove("reload");
-  //     clearInterval()
-  //   }
-  // }, 1000);
+  setInterval(function () {
+    anime -= 1;
+
+    if (anime == 0) {
+      document.getElementsByClassName("reload")[0].style.animation = "";
+      clearInterval();
+    }
+  }, 1000);
 });
-
-btn.addEventListener("click", changeBg);
 
 window.onload = changeBg();
